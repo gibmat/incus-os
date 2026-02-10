87d3d5c2045f8901ca441dd8d1f38f023553a9a0
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2026-02-10 10:29:46 -0700

    base: Activate multipath boot device in initrd
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

---
+++ mkosi.packages/incusos-initrd-utils/initrd-multipath.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Only attempt to activate root device multipath if there are duplicate WWNs present.
+
+if [ ! -e "/dev/disk/by-partlabel/esp" ]; then
+    exit
+fi
+
+ROOT_DEVICE=$(lsblk -o pkname -dn /dev/disk/by-partlabel/esp)
+ROOT_DEVICE="/dev/$ROOT_DEVICE"
+ROOT_WWN=$(lsblk -o WWN -dn "$ROOT_DEVICE")
+TOTAL_ROOT_WWNS=$(lsblk -o WWN -dn | grep -v "^$" | grep -c "$ROOT_WWN")
+
+if [ "$TOTAL_ROOT_WWNS" -gt 1 ]; then
+    multipath -a "$ROOT_DEVICE"
+    multipath -r
+
+    # Need to sleep a few seconds to allow the multipath device to become available.
+    sleep 5
+fi
